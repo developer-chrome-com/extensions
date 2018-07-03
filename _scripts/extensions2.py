762f3eeb41b858600004e5fcc6a5928839264811
panmh-popcap <panmh@popcap.com>
2018-07-03 12:20:29 +0800

    Updated parse_html

--- _scripts/extensions2.py
+++ _scripts/extensions2.py
@@ -18,6 +18,17 @@ css_list = [
 ]
 
 
+class MyHTMLParser(HTMLParser):
+    hrefs = []
+
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            if attr[0] == 'href':
+                href = attr[1]
+                href = re.sub(r'#.*$', '', href)
+                self.hrefs.append(href)
+
+
 def download_file(url, filename):
     print('Downloading {u} to {f}'.format(u=url, f=filename))
     return urlretrieve(url, filename)
@@ -29,29 +40,16 @@ def clean_dest(dest_path):
     os.mkdir(dest_path)
 
 
-def dirty_fix_for_ET(html_string):
-    # fix invalid xml
-    html_string = re.sub(r'<(link|meta|img|input|br)[^<>]*>', '', html_string)
-    html_string = re.sub(r'<([^\/\s:"]+):', r'<\1-', html_string)
-    html_string = re.sub(r':([^\/\s:"]+)>', r'-\1>', html_string)
-    return html_string
-
-
 def parse_html(index_html):
     with open(index_html, 'r') as f:
         html_string = f.read()
-    html_string = dirty_fix_for_ET(html_string)
-    html_string = re.sub(r'data-[^\s]*(>)', r'>', html_string)
-
-    root = ET.fromstring(html_string)
-    nav_el = root.find(
-        './/*[@id="gc-pagecontent"]/article/nav/section/ol/li[1]/ol/li[1]/ol')
-    href_els = nav_el.findall('.//*[@href]')
+    parser = MyHTMLParser()
+    parser.feed(html_string)
     download_list = []
-    for el in href_els:
-        href = el.attrib['href']
-        if href.startswith('/'):
-            download_list.append(href)
+    for href in parser.hrefs:
+        if href.startswith('/extensions/'):
+            if not re.search(r'(js|css|png)$', href):
+                download_list.append(href)
     return list(set(download_list))
 
 
@@ -77,14 +75,21 @@ if __name__ == '__main__':
     clean_dest(dest_path)
 
     download_file(origin_url + extensions_path, dest_path + url_suffix)
-
+    modify_css_href(dest_path + url_suffix)
     download_list = parse_html(dest_path + url_suffix)
-    for subpath in download_list:
+    downloaded = []
+
+    while len(download_list) > 0:
+        subpath = download_list.pop()
         dest_html = root_path + subpath + url_suffix
         if not os.path.exists(os.path.dirname(dest_html)):
             os.mkdir(os.path.dirname(dest_html))
         download_file(origin_url + subpath, dest_html)
+        downloaded.append(subpath)
         modify_css_href(dest_html)
+
+        download_list += list(
+            set(parse_html(dest_html)) - set(download_list) - set(downloaded))
 
     for css in css_list:
         dest_css = root_path + extensions_path + css
